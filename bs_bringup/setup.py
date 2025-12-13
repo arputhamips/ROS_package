882882a6bcc3a60e5cc4e793793acec109e4b8e6
arputhamips <159134000+arputhamips@users.noreply.github.com>
2025-12-13 04:13:30 -0600

    Update setup.py

--- bs_bringup/setup.py
+++ bs_bringup/setup.py
@@ -1,29 +1,32 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'bs_bringup'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.0.1',
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        
+        # INSTALL LAUNCH FILES
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        
+        # INSTALL CONFIG FILES (RViz files)
+        (os.path.join('share', package_name, 'config'), glob('config/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='nirmal',
-    maintainer_email='159134000+arputhamips@users.noreply.github.com',
-    description='TODO: Package description',
+    maintainer='Nirmal',
+    maintainer_email='nirmalezhil21',
+    description='Base Station tools',
     license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
